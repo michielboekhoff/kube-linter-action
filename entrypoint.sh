15e19a35f45d5f733c8a9ab3d5e1115044f68092
Michiel Alexander Boekhoff <michielalexanderb@gmail.com>
2021-04-16 00:39:06 +0100

    Change curl to wget as that's included in Alpine.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ LINT_TARGET=$2
 
 KUBE_LINTER_URL="https://github.com/stackrox/kube-linter/releases/download/$KUBE_LINTER_VERSION/kube-linter-linux.tar.gz"
 
-curl -L -s -o kube_linter.tar.gz "$KUBE_LINTER_URL"
+wget -q -O kube_linter.tar.gz "$KUBE_LINTER_URL"
 
 tar xzf kube_linter.tar.gz kube-linter
 
